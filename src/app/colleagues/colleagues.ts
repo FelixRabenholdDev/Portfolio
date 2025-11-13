6da543cb151d0125edcf152db0ba8567f35a81ee
Felix R <nikojin@gmx.net>
2025-11-13 09:20:49 +0100

    init structure

---
+++ src/app/colleagues/colleagues.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-colleagues',
+  imports: [],
+  templateUrl: './colleagues.html',
+  styleUrl: './colleagues.scss',
+})
+export class Colleagues {
+
+}
