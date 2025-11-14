dfb0eac1673c20d0c611a5186198c1b858bd8206
Felix R <nikojin@gmx.net>
2025-11-14 10:04:25 +0100

    add navbar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LandingPage } from './landing-page/landing-page';
+import { NavBar } from './nav-bar/nav-bar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LandingPage, NavBar],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
