54c2cc0b68073da6a1e84c32e323cebbd48ce5ac
Felix R <nikojin@gmx.net>
2025-11-24 09:27:04 +0100

    upd contact me

--- src/app/landing-page/contact-me/contact-me.ts
+++ src/app/landing-page/contact-me/contact-me.ts
@@ -1,11 +1,39 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-me',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './contact-me.html',
   styleUrl: './contact-me.scss',
 })
 export class ContactMe {
+  name = '';
+  email = '';
+  message = '';
+  nameError = false;
+  emailError = false;
+  messageError = false;
+  privacyAccepted = false;
+  privacyError = false;
 
+  send() {
+    this.nameError = !this.isValid(this.name);
+    this.emailError = !this.isValid(this.email);
+    this.messageError = !this.isValid(this.message);
+
+    if (!this.privacyAccepted) {
+      this.privacyError = true;
+      return;
+    }
+
+    if (this.nameError || this.emailError || this.messageError || this.privacyError) return;
+
+    this.privacyError = false;
+  }
+
+  isValid(value: string) {
+    return value.trim().length > 0;
+  }
 }
