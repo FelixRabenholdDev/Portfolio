c5588344a140f80063e34c83ed0108e846559189
Felix R <nikojin@gmx.net>
2025-11-26 13:39:21 +0100

    upd navbar burger

--- src/app/shared/nav-bar/nav-bar.ts
+++ src/app/shared/nav-bar/nav-bar.ts
@@ -1,16 +1,46 @@
-
-// import { Component } from '@angular/core';
+// import { Component, ElementRef, ViewChild } from '@angular/core';
+// import { interval, take } from 'rxjs';
 
 // @Component({
 //   selector: 'app-nav-bar',
-//   imports: [],
 //   templateUrl: './nav-bar.html',
-//   styleUrl: './nav-bar.scss',
+//   styleUrls: ['./nav-bar.scss']
 // })
-// export class NavBar {}
+// export class NavBar {
+  
+//   @ViewChild('burgerIcon', { static: false }) burgerIcon!: ElementRef<HTMLImageElement>;
+
+//   menuOpen = false;
+
+//   frames = [
+//     'assets/img/burger_menu_t0.svg',
+//     'assets/img/burger_menu_t1.svg',
+//     'assets/img/burger_menu_t2.svg',
+//     'assets/img/burger_menu_t3.svg'
+//   ];
+
+//   toggleMenu() {
+//     this.menuOpen = !this.menuOpen;
+//     this.animateBurger();
+//   }
+
+//   closeMenu() {
+//     this.menuOpen = false;
+//   }
+
+//   animateBurger() {
+//     let index = 0;
+//     interval(200)
+//       .pipe(take(this.frames.length))
+//       .subscribe(() => {
+//         this.burgerIcon.nativeElement.src = this.frames[index];
+//         index++;
+//       });
+//   }
+// }
+
 
 import { Component, ElementRef, ViewChild } from '@angular/core';
-import { interval, take } from 'rxjs';
 
 @Component({
   selector: 'app-nav-bar',
@@ -18,35 +48,33 @@ import { interval, take } from 'rxjs';
   styleUrls: ['./nav-bar.scss']
 })
 export class NavBar {
-  
+
   @ViewChild('burgerIcon', { static: false }) burgerIcon!: ElementRef<HTMLImageElement>;
+  @ViewChild('burgerBtn', { static: false }) burgerBtn!: ElementRef<HTMLButtonElement>;
 
   menuOpen = false;
 
-  frames = [
-    'assets/img/burger_menu_t0.svg',
-    'assets/img/burger_menu_t1.svg',
-    'assets/img/burger_menu_t2.svg',
-    'assets/img/burger_menu_t3.svg'
-  ];
-
   toggleMenu() {
-    this.menuOpen = !this.menuOpen;
-    this.animateBurger();
+    const btn = this.burgerBtn.nativeElement;
+
+    if (!this.menuOpen) {
+      // Menü öffnet
+      this.menuOpen = true;
+      btn.classList.add('open');
+      btn.classList.remove('closing');
+    } else {
+      // Menü schließt
+      this.menuOpen = false;
+      btn.classList.add('closing');
+      btn.classList.remove('open');
+    }
   }
 
   closeMenu() {
     this.menuOpen = false;
-  }
-
-  animateBurger() {
-    let index = 0;
-    interval(70)
-      .pipe(take(this.frames.length))
-      .subscribe(() => {
-        this.burgerIcon.nativeElement.src = this.frames[index];
-        index++;
-      });
+    const btn = this.burgerBtn.nativeElement;
+    btn.classList.add('closing');
+    btn.classList.remove('open');
   }
 }
 
