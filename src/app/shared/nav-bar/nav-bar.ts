1a49664d2859a67ad4d2e20b87f51f16055215c8
Felix R <nikojin@gmx.net>
2025-11-26 13:25:02 +0100

    upd navbar burger

--- src/app/shared/nav-bar/nav-bar.ts
+++ src/app/shared/nav-bar/nav-bar.ts
@@ -1,10 +1,52 @@
 
-import { Component } from '@angular/core';
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-nav-bar',
+//   imports: [],
+//   templateUrl: './nav-bar.html',
+//   styleUrl: './nav-bar.scss',
+// })
+// export class NavBar {}
+
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { interval, take } from 'rxjs';
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [],
   templateUrl: './nav-bar.html',
-  styleUrl: './nav-bar.scss',
+  styleUrls: ['./nav-bar.scss']
 })
-export class NavBar {}
+export class NavBar {
+  
+  @ViewChild('burgerIcon', { static: false }) burgerIcon!: ElementRef<HTMLImageElement>;
+
+  menuOpen = false;
+
+  frames = [
+    'assets/img/burger_menu_t0.svg',
+    'assets/img/burger_menu_t1.svg',
+    'assets/img/burger_menu_t2.svg',
+    'assets/img/burger_menu_t3.svg'
+  ];
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+    this.animateBurger();
+  }
+
+  closeMenu() {
+    this.menuOpen = false;
+  }
+
+  animateBurger() {
+    let index = 0;
+    interval(70)
+      .pipe(take(this.frames.length))
+      .subscribe(() => {
+        this.burgerIcon.nativeElement.src = this.frames[index];
+        index++;
+      });
+  }
+}
+
