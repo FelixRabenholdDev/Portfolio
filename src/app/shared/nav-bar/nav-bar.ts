b8571a282963894c0147dc6989adc443ea09f25a
Felix R <nikojin@gmx.net>
2025-11-27 09:34:47 +0100

    upd responsive burger

--- src/app/shared/nav-bar/nav-bar.ts
+++ src/app/shared/nav-bar/nav-bar.ts
@@ -1,45 +1,3 @@
-// import { Component, ElementRef, ViewChild } from '@angular/core';
-// import { interval, take } from 'rxjs';
-
-// @Component({
-//   selector: 'app-nav-bar',
-//   templateUrl: './nav-bar.html',
-//   styleUrls: ['./nav-bar.scss']
-// })
-// export class NavBar {
-  
-//   @ViewChild('burgerIcon', { static: false }) burgerIcon!: ElementRef<HTMLImageElement>;
-
-//   menuOpen = false;
-
-//   frames = [
-//     'assets/img/burger_menu_t0.svg',
-//     'assets/img/burger_menu_t1.svg',
-//     'assets/img/burger_menu_t2.svg',
-//     'assets/img/burger_menu_t3.svg'
-//   ];
-
-//   toggleMenu() {
-//     this.menuOpen = !this.menuOpen;
-//     this.animateBurger();
-//   }
-
-//   closeMenu() {
-//     this.menuOpen = false;
-//   }
-
-//   animateBurger() {
-//     let index = 0;
-//     interval(200)
-//       .pipe(take(this.frames.length))
-//       .subscribe(() => {
-//         this.burgerIcon.nativeElement.src = this.frames[index];
-//         index++;
-//       });
-//   }
-// }
-
-
 import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
@@ -58,12 +16,10 @@ export class NavBar {
     const btn = this.burgerBtn.nativeElement;
 
     if (!this.menuOpen) {
-      // Menü öffnet
       this.menuOpen = true;
       btn.classList.add('open');
       btn.classList.remove('closing');
     } else {
-      // Menü schließt
       this.menuOpen = false;
       btn.classList.add('closing');
       btn.classList.remove('open');
