780e0fb21c271af940809072b429ee620a0b78f4
Felix R <nikojin@gmx.net>
2026-07-22 15:20:17 +0200

    upd language switch/why me + skill

--- src/app/landing-page/my-skills/my-skills.ts
+++ src/app/landing-page/my-skills/my-skills.ts
@@ -1,11 +1,39 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TranslatePipe } from '@ngx-translate/core';
+
+interface Skill {
+  icon: string;
+  name: string;
+}
 
 @Component({
   selector: 'app-my-skills',
-  imports: [],
+  imports: [CommonModule, TranslatePipe],
   templateUrl: './my-skills.html',
   styleUrls: ['./my-skills.scss'],
 })
 export class MySkills {
+  skills: Skill[] = [
+    { icon: 'assets/img/icons/angular.svg', name: 'Angular' },
+    { icon: 'assets/img/icons/typescript.svg', name: 'TypeScript' },
+    { icon: 'assets/img/icons/javascript.svg', name: 'JavaScript' },
+    { icon: 'assets/img/icons/html.svg', name: 'HTML' },
+    { icon: 'assets/img/icons/css.svg', name: 'CSS' },
+    { icon: 'assets/img/icons/rest-api.svg', name: 'REST API' },
+    { icon: 'assets/img/icons/firebase.svg', name: 'Firebase' },
+    { icon: 'assets/img/icons/git.svg', name: 'Git' },
+    { icon: 'assets/img/icons/material-design.svg', name: 'Material Design' },
+    { icon: 'assets/img/icons/scrum.svg', name: 'Scrum' },
+    { icon: 'assets/img/icons/python.svg', name: 'Python' },
+    { icon: 'assets/img/icons/django.svg', name: 'Django' },
+    { icon: 'assets/img/icons/linux.svg', name: 'Linux' },
+    { icon: 'assets/img/icons/postgresql.svg', name: 'PostgreSQL' },
+    { icon: 'assets/img/icons/docker.svg', name: 'Docker' },
+  ];
 
+  learningSkills: Skill[] = [
+    { icon: 'assets/img/icons/java.svg', name: 'Java' },
+    { icon: 'assets/img/icons/kubernetes.svg', name: 'Kubernetes' },
+  ];
 }
--- src/app/landing-page/why-me/why-me.ts
+++ src/app/landing-page/why-me/why-me.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-why-me',
-  imports: [],
+  imports: [TranslatePipe],
   templateUrl: './why-me.html',
   styleUrls: ['./why-me.scss'],
 })
 export class WhyMe {
-
 }
