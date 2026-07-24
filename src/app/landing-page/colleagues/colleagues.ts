451df0b16183f094bc45ea767811cd9562a9ce48
Felix R <nikojin@gmx.net>
2026-07-24 12:22:33 +0200

    upd colleagues section

--- src/app/landing-page/colleagues/colleagues.ts
+++ src/app/landing-page/colleagues/colleagues.ts
@@ -1,11 +1,40 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TranslatePipe } from '@ngx-translate/core';
+
+interface Colleague {
+  name: string;
+  project: string;
+  quoteKey: string;
+  linkedin: string;
+}
 
 @Component({
   selector: 'app-colleagues',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, TranslatePipe],
   templateUrl: './colleagues.html',
   styleUrls: ['./colleagues.scss'],
 })
 export class Colleagues {
-
+  colleagues: Colleague[] = [
+    {
+      name: 'Paul Angeles Chaquire',
+      project: 'Kochwelt',
+      quoteKey: 'COLLEAGUES.PAUL.QUOTE',
+      linkedin: '#',
+    },
+    {
+      name: 'Jürgen Malinowski',
+      project: 'Join',
+      quoteKey: 'COLLEAGUES.JUERGEN.QUOTE',
+      linkedin: '#',
+    },
+    {
+      name: 'Emil Marsal',
+      project: 'Join',
+      quoteKey: 'COLLEAGUES.EMIL.QUOTE',
+      linkedin: '#',
+    },
+  ];
 }
