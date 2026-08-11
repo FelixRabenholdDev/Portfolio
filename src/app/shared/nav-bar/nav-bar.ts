8af06063162d373939b935b343602ad2162b832f
Felix R <nikojin@gmx.net>
2026-08-12 00:14:50 +0200

    upd navbar logo

--- src/app/shared/nav-bar/nav-bar.ts
+++ src/app/shared/nav-bar/nav-bar.ts
@@ -1,14 +1,23 @@
 import { Component, inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 import { TranslatePipe } from '@ngx-translate/core';
 import { LanguageService } from '../services/language.service';
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [TranslatePipe],
+  imports: [RouterLink, TranslatePipe],
   templateUrl: './nav-bar.html',
   styleUrls: ['./nav-bar.scss']
 })
 export class NavBar {
   languageService = inject(LanguageService);
+  private router = inject(Router);
+
+  onLogoClick(event: MouseEvent): void {
+    if (this.router.url === '/') {
+      event.preventDefault();
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    }
+  }
 }
 
